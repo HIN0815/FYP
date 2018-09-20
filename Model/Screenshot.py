6de9042013388dcd4397fffcb0deaa4962dc2e86
tommy1008 <43204725+tommy1008@users.noreply.github.com>
2018-09-20 19:28:30 +0800

    Add files via upload

--- Model/Screenshot.py
+++ Model/Screenshot.py
@@ -1,16 +1,10 @@
 import time
 import pyautogui
-import numpy as np
-import pyautogui
-import imutils
-import cv2
+
 import time
-import platform
 
 
-
-
-class window():
+class Screenshot():
     # Get the time and date
     timestr = time.strftime("%Y%m%d-%H%M%S")
     # Take screenshot
@@ -18,17 +12,3 @@ class window():
 
     # Save the image
     pic.save(str(timestr) + '.png')
-
-
-class linux():
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    image = pyautogui.screenshot()
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    cv2.imwrite(str(timestr) + '.png', image)
-
-
-class Screenshot():
-    if (platform.system() == 'Windows'):
-        window()
-    else:
-        linux()
