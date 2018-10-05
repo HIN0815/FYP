cdb475fd64098a2b3dc5ad5ac8fad050be90d6d6
tommy1008 <43204725+tommy1008@users.noreply.github.com>
2018-10-05 10:03:41 +0800

    Update test.py

--- Model/test.py
+++ Model/test.py
@@ -1,28 +1,19 @@
-import  requests
-import boto3
-import json
-import os
-running_process=[]
-data = json.dumps(running_process).encode('utf8')
+import requests
 
 
-s3 = boto3.resource('s3')
 
-response = requests.post("https://yo1gmy11ah.execute-api.ap-southeast-1.amazonaws.com/Prod/process", data=data,
+response = requests.post("https://yo1gmy11ah.execute-api.ap-southeast-1.amazonaws.com/Prod/process",
                          headers={"x-api-key": "9f0043addf877477c0e2aed76284ba37d63e8658f551c2c7b2e2102d"})
-with open('D:/Screenshot2.jpg', 'rb') as z:
-  a= z.read()
 
 
-
-
+files = {'file': ('D:\\Screenshot.png', open('D:\\20181005-084629.png', 'rb'), 'image/png', {'Expires': '0'})}
 if response.ok:
-     print(response.json())
-     upload=response.json()
+        print(response.json())
+        upload = response.json()
 
-     r=requests.post(upload, files=a)
-     print(r.text)
+        r = requests.post(upload, files=files)
+        print(r.text)
 
 else:
-    print(response.status_code)
-    print(response.reason)
+        print(response.status_code)
+        print(response.reason)
