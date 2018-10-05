ecfb2da42f352fc138161f1fcd2e0b4748c0410b
tommy1008 <43204725+tommy1008@users.noreply.github.com>
2018-10-05 10:04:13 +0800

    Update test.py

--- Model/test.py
+++ Model/test.py
@@ -6,7 +6,7 @@ response = requests.post("https://yo1gmy11ah.execute-api.ap-southeast-1.amazonaw
                          headers={"x-api-key": "9f0043addf877477c0e2aed76284ba37d63e8658f551c2c7b2e2102d"})
 
 
-files = {'file': ('D:\\Screenshot.png', open('D:\\20181005-084629.png', 'rb'), 'image/png', {'Expires': '0'})}
+files = {'file': ('D:\\Screenshot.png', open('D:\\Screenshot.png', 'rb'), 'image/png', {'Expires': '0'})}
 if response.ok:
         print(response.json())
         upload = response.json()
